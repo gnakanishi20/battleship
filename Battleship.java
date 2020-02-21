8b5587d4161643454d2f48559c2fb3e40af89c9c
gnakanishi20 <gnakanishi20@punahou.edu>
2020-02-21 18:38:25 +0000

    first commit

---
+++ Battleship.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class battleship
+{
+    boolean hit = false;
+     
+    public board()
+    {
+        // initialize an array, 10x10 grid
+        // set each space to false
+        //create 5 ships with lengths 2,3,3,4,5 
+        //create by changing spaces that it occupies into true
+        //pick between 1 or 2 to determine whether its horizontal or vertical
+        // if the boat hits any spaces that is true, move the whole boat one to the side and one up keep going until either hit the edge or is all true. If not possible randomize again
+        //
+    }
+    //initializes board
+    //take in a array value?
+    //iterate through board
+    //check if hit or not?
+    public boardHit()
+    {
+        
+    }
+}
