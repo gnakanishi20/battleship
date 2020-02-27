7f51332b9b2e2a51b56b5d6c3c8d21a63c567959
gnakanishi20 <gnakanishi20@punahou.edu>
2020-02-27 06:32:49 +0000

    rewrote the board code like we talked about in class. Still need to go through bugs, I know syntax is not perfect yet, board code is not final yet. Also worked on the game running.

--- Battleship.java
+++ Battleship.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class battleship
 {
     boolean hit = false;
-    // boolean [][] first = boolean int [10][10];
+    boolean [][] first = boolean int [10][10];
      
     public board()
     {
@@ -21,104 +21,75 @@ public class battleship
         
         int ColoumnTwo = ColoumnTwo.randomColoumn();
         int RowTwo = RowTwo.randomRow();
-        first [RowTwo][ColoumnTwo] = 1;
+        int d = d.RanDirrection()
+        int TwoLength = 2;
         // picks dirrection of the boat, if dirrection = 1 then vertical if dirrection = 2 then horizontal
-        int dirrection = dirrection.RanDirrection()
         
-        if(dirrection == 1)
+        // if(PlaceShip(TwoLength, RowTwo, ColoumnTwo, d) == true)
+        // {
+        //     if(d == 1)
+        //     {
+        //         for(int i = 0, i<TwoLength,i++)
+        //         {
+        //             first[RowTwo + i][ColoumnTwo];
+        //         }
+        //     }
+        //     else
+        //     {
+        //         for(int i = 0, i<TwoLength,i++)
+        //         {
+        //             first[RowTwo][ColoumnTwo + 1];
+        //         }
+        //     }
+        // }
+        
+        for(int i = 1, i<3, i++)
         {
-            //makes sure boat wont go off grid
-            if(ColoumnTwo == 10)
+            int d = d.RanDirrection()
+            int Coloumn = (int)(Math.random() * (10 - i));
+            int Row = (int)(Math.random() * (10 - i));
+            int Length = 2 + i;
+            if(PlaceShip(int length, int x, int y, int d))
             {
-                first [RowTwo][ColoumnTwo - 1] = 1;
+                ChangeBoard(int length, int x, int y, int d)
             }
             else
             {
-                first [RowTwo][ColoumnTwo + 1] = 1; 
-            }
-        }
-        else
-        {
-            //makes sure boat wont go off grid
-            if(RowTwo == 10)
-            {
-                first [RowTwo - 1][ColoumnTwo] = 1;
-            }
-            else
-            {
-                first [RowTwo + 1][ColoumnTwo] = 1;
-            }
-        }
-        
-        // ship 3
-        int ColoumnThree = ColoumnThree.randomColoumn();
-        int RowThree = RowThree.randomRow();
-        
-        int DirrectionThree = DirrectionThree.RanDirrection();
-        
-        do
-        {
-            int ColoumnThree = ColoumnThree.randomColoumn();
-            int RowThree = RowThree.randomRow();
-        }
-        while(first [RowThree][ColoumnThree] == 1)
-        
-        
-        
-        if(DirrectionThree == 1)
-        {
-                //makes sure boat wont go off grid
-            if(ColoumnThree >= 8)
-            {
-                if(first[RowThree][ColoumnThree] == 0 && first[RowThree][ColoumnThree - 1] == 0 && first[RowThree][ColoumnThree - 2] == 0)
+                while(!PlaceShip(int length, int x, int y, int d))
                 {
-                    for(int i = 0, i<3, i++)
-                    {
-                        first [RowThree][ColoumnThree - i] = 1;
-                    }
+                    int d = d.RanDirrection()
+                    int Coloumn = (int)(Math.random() * (10 - i));
+                    int Row = (int)(Math.random() * (10 - i));
+                    int Length = 2 + i;
                 }
-                else if(ColoumnTwo == 9)
-                {
-                    for(int i = 0, i<3, i++)
-                    {
-                        first [RowThree - 1][ColoumnThree - i] = 1;
-                    }
-                }
-                else
-                {
-                    if(first[RowThree][ColoumnThree] == 0 && first[RowThree][ColoumnThree - 1] == 0 && first[RowThree][ColoumnThree - 2] == 0)
-                    {
-                        for(int i = 0, i<3, i++)
-                        {
-                            first [RowThree - 2][ColoumnThree - i] = 1;
-                        }
-                    }
-                }
-            }
-            else 
-            {
-                if(first[RowThree][ColoumnThree] == 0 && first[RowThree][ColoumnThree + 1] == 0 && first[RowThree][ColoumnThree + 2] == 0)
-                {
-                    first [RowThree][ColoumnThree + i]  = 1; 
-                }
-                
+                ChangeBoard(int length, int x, int y, int d)
             }
             
         }
-        else
+        
+        for(int i = 2, i<5, i++)
         {
-            //makes sure boat wont go off grid
-            if(RowTwo == 10)
+            int d = d.RanDirrection()
+            int Coloumn = (int)(Math.random() * (10 - i));
+            int Row = (int)(Math.random() * (10 - i));
+            int Length = 2 + i;
+            if(PlaceShip(int length, int x, int y, int d))
             {
-                first [RowThree - i][ColoumnThree] = 1;
+                ChangeBoard(int length, int x, int y, int d)
             }
             else
-            {   
-                first [RowThree + i][ColoumnThree] = 1;
+            {
+                while(!PlaceShip(int length, int x, int y, int d))
+                {
+                    int d = d.RanDirrection()
+                    int Coloumn = (int)(Math.random() * (10 - i));
+                    int Row = (int)(Math.random() * (10 - i));
+                    int Length = 2 + i;
+                }
+                ChangeBoard(int length, int x, int y, int d)
             }
+            
         }
-        
-        
         //create by changing spaces that it occupies into 1
         //pick between 1 or 2 to determine whether its horizontal or vertical
         // if the boat hits any spaces that is 1, move the whole boat one to the side and one up keep going until either hit the edge or is all 1. If not possible randomize again
@@ -140,19 +111,52 @@ public class battleship
         }
     }
     
-    public randomColoumn()
+    //run until true, b=put in another function
+    
+    public boolean PlaceShip(int length, int x, int y, int d)
     {
-        int Coloumn = (int)(Math.random() * 10);
-        return Coloumn;
+        int placed = 0;
+        while(placed < length)
+        {
+            if(board[x][y] == 0)
+            {
+                if(d == 1)
+                {
+                    x++;
+                }
+                else
+                {
+                    y++;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return true;
     }
     
-    public randomRow()
+    public static ChangeBoard(int length, int x, int y, int d)
     {
-        int Row = (int)(Math.random() * 10);
-        return Row;
+        if(d == 1)
+        {
+            for(int i = 0, i<length, i++)
+            {
+                first[x + i][y];
+            }
+        }
+        else
+        {
+            for(int i = 0, i<TwoLength,i++)
+            {
+                first[x][y + 1];
+            }
+        }
     }
     
-    public RanDirrection()
+    
+    public int RanDirrection()
     {
         int dirrection = (int)(Math.random() * 2);
         return dirrection;
