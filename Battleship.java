32f4a52aa6093baa1d0d31de49e3ef52238b2081
gnakanishi20 <gnakanishi20@punahou.edu>
2020-02-27 18:50:38 +0000

    changed the organization of my code to make it run and make more sense. Will work on syntax, not quite done with the bugs and coding yet

--- Battleship.java
+++ Battleship.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 
-public class battleship
+public class Battleship
 {
     boolean hit = false;
-    boolean [][] first = boolean int [10][10];
-     
-    public board()
+    int[][] first = new int[10][10];
+    
+    public Battleship()
     {
         // initialize an array, 10x10 grid, already declared 0?
         // 1 = ship is there, 0 = no ship
@@ -20,7 +20,7 @@ public class battleship
         // int lastRoll2 = last2.roll();
         
         int ColoumnTwo = ColoumnTwo.randomColoumn();
-        int RowTwo = RowTwo.randomRow();
+        int RowTwo = RowTwo.randomRow();;
         int d = d.RanDirrection()
         int TwoLength = 2;
         // picks dirrection of the boat, if dirrection = 1 then vertical if dirrection = 2 then horizontal
@@ -48,7 +48,8 @@ public class battleship
             int d = d.RanDirrection()
             int Coloumn = (int)(Math.random() * (10 - i));
             int Row = (int)(Math.random() * (10 - i));
-            int Length = 2 + i;
+            int Length = 1 + i;
+            
             if(PlaceShip(int length, int x, int y, int d))
             {
                 ChangeBoard(int length, int x, int y, int d)
@@ -60,7 +61,7 @@ public class battleship
                     int d = d.RanDirrection()
                     int Coloumn = (int)(Math.random() * (10 - i));
                     int Row = (int)(Math.random() * (10 - i));
-                    int Length = 2 + i;
+                    int Length = 1 + i;
                 }
                 ChangeBoard(int length, int x, int y, int d)
             }
@@ -72,7 +73,8 @@ public class battleship
             int d = d.RanDirrection()
             int Coloumn = (int)(Math.random() * (10 - i));
             int Row = (int)(Math.random() * (10 - i));
-            int Length = 2 + i;
+            int Length = 1 + i;
+            
             if(PlaceShip(int length, int x, int y, int d))
             {
                 ChangeBoard(int length, int x, int y, int d)
@@ -84,7 +86,7 @@ public class battleship
                     int d = d.RanDirrection()
                     int Coloumn = (int)(Math.random() * (10 - i));
                     int Row = (int)(Math.random() * (10 - i));
-                    int Length = 2 + i;
+                    int Length = 1 + i;
                 }
                 ChangeBoard(int length, int x, int y, int d)
             }
@@ -94,6 +96,103 @@ public class battleship
         //pick between 1 or 2 to determine whether its horizontal or vertical
         // if the boat hits any spaces that is 1, move the whole boat one to the side and one up keep going until either hit the edge or is all 1. If not possible randomize again
         //
+        return first;
+    }
+     
+    public void play()
+    {
+        boolean win = false;
+        
+        board create[][] = new board();
+        
+        
+        Scanner kb = new Scanner(System.in);
+        
+        System.out.println("Welcome to battleship!");
+        //ask for the player to enter space to shoot 
+        System.out.println("What row do you want to shoot in?");
+        int PickRow = kb.nextInt();
+        
+        do
+        {
+            System.out.println("Row number must be less then 10, pick again!");
+            PickRow = kb.nextInt();
+        }
+        while(PickRow > 10);
+        
+        System.out.println("What Coloumn do you want to shoot in?");
+        int PickColoumn = kb.nextInt();
+        
+        do
+        {
+            System.out.println("Coloumn number must be less then 10, pick again!");
+            PickColoumn = kb.nextInt();
+        }
+        while(PickColoumn > 10);
+        //since its going to be an array how do I take a command line function and translate that into the array
+        // maybe have two command line asks first the column then the PickRow?
+        
+        while(win == false)
+        {
+        // now with the array location check to see if the space is marked true or false
+            if(first [PickRow][PickColoumn] == true)
+            {
+                System.out.println("Hit!");
+                first[PickRow][PickColoumn] = 0;
+                identical[PickRow][PickColoumn] = 1;
+            }
+            else
+            {
+                System.out.println("Miss!");
+                identical[PickRow][PickColoumn] = 2;
+            }
+        // if marked true print hit and change the value on orgiginal board to false
+        // on the identical board change the print value to 1 to make it print an X
+            // will I implement something about sinking a ship?! how would I do that 
+            
+        // if marked false print miss!
+            // Code a miss into the identical board, the print line will change it into the miss symbol, which will be a space "  " will represent miss, value will b 2
+                // a regular space will be printed as a O and be represented as the value 3
+            
+        // maybe create an identical board that is the one that is printed on the screen
+        // this printed board will have the hit and misses that the player guessed
+        // yes I will do that 
+            int IdentPickRow = 0;
+            int IdentPickColoumn = 0;
+        
+            for(int i = 0; i<10; i++)
+            {
+                for(int j = 0; j<10; j++)
+                {
+                    if(identical[IdentPickRow + j][IdentPickColoumn + i] == 1)
+                    {
+                        System.out.println("X");
+                    }
+                    else if(identical[IdentPickRow + j][IdentPickColoumn + i] == 2)
+                    {
+                        System.out.println(" ");
+                    }
+                    else
+                    {
+                        System.out.println("O");
+                    }
+                }
+            }
+        
+        // print identical board, maybe create a different function within the driver
+        
+        //Check if the game is won
+            if(dub())
+            {
+                System.out.println("Congratulations you won!");
+                win = true;
+            }
+            // do this by checking if the original board is all false
+            // if all false then keep running the game
+            // put the whole code withing a while loop like in die 
+            // if game is won, set the boolean to true and print out a winning statement
+        }
+        
     }
     
     public IdentBoard()
@@ -143,23 +242,42 @@ public class battleship
         {
             for(int i = 0, i<length, i++)
             {
-                first[x + i][y];
+                first[x + i][y] = 1;
             }
         }
         else
         {
             for(int i = 0, i<TwoLength,i++)
             {
-                first[x][y + 1];
+                first[x][y + i] = 1;
             }
         }
     }
     
+    // 0 == space, 1 == occupied
     
     public int RanDirrection()
     {
         int dirrection = (int)(Math.random() * 2);
         return dirrection;
+    }
+    
+    public boolean dub()
+    {
+        // I am using var to check if everything in the interval is 0
+        var result = true;
+        for(var i=0; i<10; i++)
+        {
+            for(var j=0; i<10; i++)
+            {
+                if(first[i][j] = 1)
+                {
+                    result = false;
+                    break;
+                }
+            }
+        }
+        return true;
     }
     //initializes board
     //take in a array value?
