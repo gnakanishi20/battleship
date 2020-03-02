9067760f03fe6bb1a7142525333bac4351dbc148
gnakanishi20 <44100295+gnakanishi20@users.noreply.github.com>
2020-03-01 14:10:24 -1000

    runs the code for the game

---
+++ BattleshipDriver.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class BattleshipDriver
+{
+    
+    public static void main(String[] args)
+    {
+        Battleship b = new Battleship();
+        b.play();
+    }
+    
+}
+        
+    
+    
+    
+    // a hit will be represented with 1 and print X, a miss will be represented as 2 and print " ", and a regular space will be 3 and print O
+    // seperate print function
+    // initialize a copy of the board, not entirely sure where to do this
+    // with this copy set all values to 3 so it will print O
+    // then have the print statement interate through all of the values printing as it goes,
+    // for loop?
+        // inside the for loop have the conditions
+        // if 1 print X, 2 print " ", 3 print O
+    
+    
+    // should I add a pause like in die? right now leaning towards no
+    
+    // make a seperate function that itterates through the identical board and gives every space in the array the value of 3
+
+    
